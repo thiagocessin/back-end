6c2bbe166700e583ff05bd855da4c09b15374c71
thiagocessin <thiagoacessin@gmail.com>
2021-06-27 13:21:55 -0300

    Paginação

--- spring-boot-ionic/src/main/java/com/demo/services/CategoriaService.java
+++ spring-boot-ionic/src/main/java/com/demo/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.demo.domain.Categoria;
@@ -47,6 +50,16 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 
+	}
+	
+	//consulta para recuperar página de dados
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy, String direction){
+		
+		//obj que prepara as informações para que eu faça a conulta que me retorna a página de dados
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 
 }

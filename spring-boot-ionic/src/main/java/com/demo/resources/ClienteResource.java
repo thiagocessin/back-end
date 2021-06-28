457e8bbfdc5e531108c228a221ed4754780b0768
thiagocessin <thiagoacessin@gmail.com>
2021-06-28 09:10:41 -0300

    Inser e delete cliente

--- spring-boot-ionic/src/main/java/com/demo/resources/ClienteResource.java
+++ spring-boot-ionic/src/main/java/com/demo/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.demo.domain.Cliente;
 import com.demo.dto.ClienteDTO;
+import com.demo.dto.ClienteNewDTO;
 import com.demo.services.ClienteService;
 
 @RestController
@@ -72,6 +75,16 @@ public class ClienteResource {
 				.map(obj -> new ClienteDTO(obj));
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
--- spring-boot-ionic/src/main/java/com/demo/services/ClienteService.java
+++ spring-boot-ionic/src/main/java/com/demo/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.demo.domain.Cidade;
 import com.demo.domain.Cliente;
+import com.demo.domain.Endereco;
+import com.demo.domain.enums.TipoCliente;
 import com.demo.dto.ClienteDTO;
+import com.demo.dto.ClienteNewDTO;
 import com.demo.repositories.ClienteRepository;
+import com.demo.repositories.EnderecoRepository;
 import com.demo.services.exceptions.DataIntegrityException;
 import com.demo.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +28,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -36,6 +46,16 @@ public class ClienteService {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
+	}
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		
+		enderecoRepository.saveAll(obj.getEnderecos());
+		
+		return obj;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
@@ -64,6 +84,27 @@ public class ClienteService {
 	}
 
 	public Cliente fromDTO(ClienteDTO dto) {
-		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(),null, null);
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO dto) {
+		Cliente cli = new Cliente(null, dto.getNome(), dto.getEmail(), dto.getCpfOuCnpj(),
+				TipoCliente.toEnum(dto.getTipo()));
+		Cidade cid = new Cidade(dto.getCidadeId(), null, null);
+
+		Endereco end = new Endereco(null, dto.getLogradouro(), dto.getNumero(), dto.getComplemento(), dto.getBairro(),
+				dto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+
+		cli.getTelefones().add(dto.getTelefone1());
+		if (dto.getTelefone2() != null) {
+			cli.getTelefones().add(dto.getTelefone2());
+		}
+		if (dto.getTelefone3() != null) {
+			cli.getTelefones().add(dto.getTelefone3());
+		}
+
+		return cli;
+
 	}
 }

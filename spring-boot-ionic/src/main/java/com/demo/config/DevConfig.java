eb15f846a57b8a7ff9a27098f715603540c0f4bf
thiagocessin <thiagoacessin@gmail.com>
2021-06-30 11:21:39 -0300

    Configuraçã envio projeto para produção

--- spring-boot-ionic/src/main/java/com/demo/config/DevConfig.java
+++ spring-boot-ionic/src/main/java/com/demo/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,10 +15,16 @@ public class DevConfig {
 	@Autowired
 	private DBService dbService;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
 	public boolean instantiateDataBase() {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
 		dbService.initiate();
 		return true;
 	}
-	
+
 }
